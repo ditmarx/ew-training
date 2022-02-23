3d7896b7ec7fdbee99f972f76106367d161aed22
dmytro.batazov <dmytro.batazov@extrawest.com>
2022-02-23 17:46:32 +0200

    Add LaunchMap component with Google Maps integration

--- src/components/common/LaunchMap/LaunchMap.styles.ts
+++ src/components/common/LaunchMap/LaunchMap.styles.ts
@@ -1,10 +1,17 @@
 const styles = {
     outerBox: {
-        border: 1,
         backgroundColor: 'rgba(0, 0, 0, 0.1)',
         width: '100%',
         height: 0,
         paddingBottom: '56.25%',
+        position: 'relative' as const,
+    },
+    mapContainer: {
+        width: '100%',
+        height: '100%',
+        position: 'absolute' as const,
+        top: 0,
+        left: 0,
     },
 };
 
--- src/components/common/LaunchMap/LaunchMap.tsx
+++ src/components/common/LaunchMap/LaunchMap.tsx
@@ -1,11 +1,40 @@
 import { Box } from '@mui/material';
+import { GoogleMap, useLoadScript, Marker, } from '@react-google-maps/api';
 import styles from './LaunchMap.styles';
 
+const center = {
+    lat: 46.9708296,
+    lng: 31.9894319,
+};
+
 const LaunchMap = () => {
+    const { isLoaded, loadError } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY ?? '',
+    });
+
+    if (loadError) {
+        return <>Map cannot be loaded right now, sorry.</>
+    }
+
+    if (!isLoaded) {
+        return <>Loading maps</>;
+    }
+
+    // return isLoaded ? renderMap() : <Spinner />
 
     return (
         <Box sx={styles.outerBox}>
-            
+            <GoogleMap
+                mapContainerStyle={styles.mapContainer}
+                clickableIcons={true}
+                center={center}
+                zoom={12}
+            >
+                <Marker
+                    onLoad={console.log}
+                    position={center}
+                />
+            </GoogleMap>
         </Box>
     );
 };
