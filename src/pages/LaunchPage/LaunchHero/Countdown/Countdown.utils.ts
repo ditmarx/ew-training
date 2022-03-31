acb408f3a81e6cc5ae03ae96b301ea3a1a6870ef
my-name <me@server.mail>
2022-03-31 23:54:40 +0300

    Refactor Countdown's helper-function using 'dayjs'

--- src/pages/LaunchPage/LaunchHero/Countdown/Countdown.utils.ts
+++ src/pages/LaunchPage/LaunchHero/Countdown/Countdown.utils.ts
@@ -1,20 +1,19 @@
-const secInMinute = 60;
-const secInHour = 60 * secInMinute;
-const secInDay = 24 * secInHour;
+import dayjs from "dayjs";
+import durationPlugin from "dayjs/plugin/duration";
+
+dayjs.extend(durationPlugin);
 
 const withLeadingZero = (n: number): string => {
-    return String(n).padStart(2, '0');
+    return Math.floor(n).toString().padStart(2, '0');
 };
 
 export const formatCountdown = (ms: number | undefined): string => {
     if (typeof ms !== 'number') return '-- : -- : -- : --';
     if (ms < 0) return 'LAUNCHED';
 
-    const timeSec = Math.floor(ms / 1000);
-    const days = withLeadingZero(Math.floor(timeSec / secInDay));
-    const hours = withLeadingZero(Math.floor((timeSec % secInDay) / secInHour));
-    const minutes = withLeadingZero(Math.floor((timeSec % secInHour) / secInMinute));
-    const seconds = withLeadingZero(Math.floor(timeSec % secInMinute));
+    const duration = dayjs.duration(ms);
+    const days = withLeadingZero(duration.asDays());
+    const time = duration.format(' : HH : mm : ss');
 
-    return `${days} : ${hours} : ${minutes} : ${seconds}`;
+    return days + time;
 };
